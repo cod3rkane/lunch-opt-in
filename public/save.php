2e05f3bc15fc7d04a019134e29e1a780086a86f2
cod3rkane <j-lopes@outlook.com>
2016-07-11 18:34:39 -0300

    update save.php

--- public/save.php
+++ public/save.php
@@ -5,20 +5,31 @@ $store      = new \Sta\SnackOptIn\Store();
 $storeItems = $store->fetchAll();
 //$gravatar   = new \Sta\SnackOptIn\Gravatar();
 
-$Person = new \Sta\SnackOptIn\Entity\Person();
-$Person->setEmail($_POST['person']);
-$schedule = json_decode($_POST['schedule']);
-$schedules = [];
+// @todo precisamos testar todos os argumentos para ver se existe. se não existir retornar alguma msg.
 
-foreach ($schedule as $item) {
-    $Schedule = new \Sta\SnackOptIn\Entity\Schedule();
-    $Schedule->setDate(new DateTime($item->date));
-    $Schedule->setGoing($item->going);
-    $Schedule->setGuests((int)($item->guests > 0 ? $item->guests : 0));
-    array_push($schedules, $Schedule);
+if (!$Person = $store->byEmail($_POST['person'])) {
+    $Person = new \Sta\SnackOptIn\Entity\Person();
+    $Person->setEmail($_POST['person']);
 }
 
-$Person->setSchedule($schedules);
+$data = json_decode($_POST['schedule']);
+
+if (!array_key_exists('date', (array) $data)) {
+    foreach ($data as $item) {
+        $Schedule = new \Sta\SnackOptIn\Entity\Schedule();
+        $Schedule->setDate(new DateTime($item->date));
+        $Schedule->setGoing($item->going);
+        $Schedule->setGuests((int)($item->guests > 0 ? $item->guests : 0));
+        $Person->addSchedule($Schedule);
+    }
+} else {
+    $schedule = new \Sta\SnackOptIn\Entity\Schedule();
+    $schedule->setDate(new DateTime($data->date));
+    $schedule->setGuests($data->guests);
+    $schedule->setGoing($data->going);
+    $Person->addSchedule($schedule);
+}
+
 $store->addItem($Person);
 
 header('Content-type: application/json');
