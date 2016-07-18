3cdda1b9ee98eeab1b1e0450c62c810c39e29218
cod3rkane <j-lopes@outlook.com>
2016-07-18 15:45:44 -0300

    Update GetItems

--- public/GetItems.php
+++ public/GetItems.php
@@ -10,21 +10,30 @@ require __DIR__ . '/../vendor/autoload.php';
 $store      = new \Sta\SnackOptIn\Store();
 $storeItems = $store->fetchAll();
 $items = [];
-
+$now = new DateTime('now');
+$now->setTime(0,0,0);
 foreach ($storeItems as $person) {
     $schedules = [];
     foreach ($person->getSchedule() as $schedule) {
-        array_push($schedules, [
-            'date' => $schedule->getDate(),
-            'going' => $schedule->getGoing(),
-            'guests' => $schedule->getGuests()
-        ]);
+        $schedule->getDate()->setTime(0,0,0);
+        $schedule->getDate()->setTimezone(new DateTimeZone('America/Sao_Paulo'));
+        if ($schedule->getDate()->getTimestamp() >= $now->getTimestamp()) {
+            array_push($schedules, [
+                'date' => $schedule->getDate(),
+                'going' => $schedule->getGoing(),
+                'guests' => $schedule->getGuests()
+            ]);
+        } else {
+            $person->removeSchedule($schedule);
+        }
     }
 
     array_push($items, [
         'email' => $person->getEmail(),
         'schedule' => $schedules
     ]);
+
+    $store->addItem($person);
 }
 
 header('Content-type: application/json');
